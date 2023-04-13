2d43fc8cd9d90766e2f6ebaddbfadf2fdbe1cb3a
Hans Raaf <hara@oderwat.de>
2023-04-13 22:13:58 +0200

    Added "WithSkip" functions to wfl

--- wfl/file_line_errors.go
+++ wfl/file_line_errors.go
@@ -9,10 +9,17 @@ import (
 // Error wraps the error message and puts file and line in the front of it.
 // Notice: If error is nil it will just return nil
 func Error(err error) error {
+	return ErrorWithSkip(err, 2)
+}
+
+// ErrorWithSkip wraps the error message and puts file and line in the front of it.
+// The argument skip is the number of stack frames to ascend
+// Notice: If error is nil it will just return nil
+func ErrorWithSkip(err error, skip int) error {
 	if err == nil {
 		return nil
 	}
-	_, file, line, _ := runtime.Caller(1)
+	_, file, line, _ := runtime.Caller(skip)
 	return fmt.Errorf("%s:%d: %w", filepath.Base(file), line, err)
 }
 
@@ -20,7 +27,15 @@ func Error(err error) error {
 // Notice: If there are error types in the arguments and all of them are nil,
 // the function will nil
 func Errorf(format string, args ...any) error {
-	_, file, line, _ := runtime.Caller(1)
+	return ErrorfWithSkip(2, format, args)
+}
+
+// ErrorfWithSkip wraps the Errorf output and puts file and line in the front of it.
+// The argument skip is the number of stack frames to ascend
+// Notice: If there are error types in the arguments and all of them are nil,
+// the function will nil
+func ErrorfWithSkip(skip int, format string, args ...any) error {
+	_, file, line, _ := runtime.Caller(skip)
 	format = "%s:%d: " + format
 	nargs := make([]any, 0, len(args)+2)
 	// not sure if that should be without path,  but having the path may expose private data?
